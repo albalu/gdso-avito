59b129b085244560a94054039e2e76d562aebcd1
Alireza <alireza.faghaninia@gmail.com>
2018-07-26 10:47:15 -0700

    added featurize_date_col and docs + ignore pandas warnings

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier, TPOTRegressor
+pd.options.mode.chained_assignment = None
 
 russian_2017_holidays = pd.to_datetime([
     '2017-01-01', # new years day
@@ -38,12 +39,32 @@ def is_russian_2017_holiday(date):
         date (datetime): datetime object to be checked.
 
     Returns (bool):
-        is holiday (True) or not? (False)?
+        is holiday (True) or not (False)?
     """
     return int(date in russian_2017_holidays)
 
 
-def generate_periods_features(df):
+def featurize_date_col(df, col, remove_when_done=True):
+    """
+    Returns some information (features) about the column date, added to the
+    base dataframe.
+
+    Args:
+        df (pandas.DataFrame): original input dataframe
+        col_name (str): column name or feature. It must be a date object dtype
+        remove_when_done (bool): remove the original date column
+
+    Returns (pandas.DataFrame):
+    """
+    df['{}_isholiday'.format(col)] = df[col].apply(is_russian_2017_holiday)
+    df['{}_wday'.format(col)] = df[col].apply(lambda x: x.dayofweek)
+    df['{}_yday'.format(col)] = df[col].apply(lambda x: x.dayofyear)
+    if remove_when_done:
+        df = df.drop(col, axis=1)
+    return df
+
+
+def generate_periods_features(df, remove_col_when_done=True):
     """
     Given a dataframe with features item_id, activation_date, date_from, date_to,
     it generates features that help the training the machine learning (ML) model.
@@ -52,29 +73,27 @@ def generate_periods_features(df):
     Args:
         df (pandas.DataFrame): must include 'item_id', 'activation_date',
             'date_from', 'date_to' columns.
+        remove_col_when_done (bool): when featurizing the date columns, whether
+            to remove them afterwards or not.
 
     Returns (pandas.DaraFrame): featurized input with the original columns dropped
     """
     for col in ['item_id', 'activation_date', 'date_from', 'date_to']:
         assert col in df
     null_idx = df['activation_date'].isnull()
-    # checked train and test and date_from, date_to don't have nulls
+    df['activation_date'].loc[null_idx] = df['date_from'].loc[null_idx]
     assert df.isnull().sum().sum() == 0
-    df['activation_date'][null_idx] = df['date_from'][null_idx]
     for col in ['activation_date', 'date_from', 'date_to']:
-        df[col] = df[col].apply(pd.to_datetime, yearfirst=True)
-    assert len(df) == len(df.drop_duplicates('item_id'))
-    assert (df['date_to'] > df['date_from']).all()
+        df[col] = pd.to_datetime(df[col], format='%Y-%m-%d')
+    assert (df['date_to'] >= df['date_from']).all()
     assert (df['date_from'] >= df['activation_date']).all()
 
     # feature generation
     df['days_to_publish'] = (df['date_from']-df['activation_date']).dt.days
     df['days_online'] = (df['date_to'] - df['date_from']).dt.days
     for col in ['activation_date', 'date_from', 'date_to']:
-        df['{}_isholiday'.format(col)] = df[col].apply(is_russian_2017_holiday)
-        df['{}_dayofweek'.format(col)] = df[col].apply(lambda x: x.dayofweek)
-        df['{}_dayofyear'.format(col)] = df[col].apply(lambda x: x.dayofyear)
-    return df.drop(['activation_date', 'date_from', 'date_to'], axis=1)
+        df = featurize_date_col(df, col, remove_when_done=remove_col_when_done)
+    return df
 
 
 def TpotAutoml(mode, feature_names=None, **kwargs):
