ac8e4685e8cbc4b0c9570e09f76d5a5e964777c6
Alireza <alireza.faghaninia@gmail.com>
2018-07-28 23:31:45 -0700

    update get_aggregate_periods test

--- test_utils.py
+++ test_utils.py
@@ -4,15 +4,20 @@ from __future__ import unicode_literals, absolute_import
 import pandas as pd
 import os
 import unittest
-from utils import generate_periods_features
+from utils import get_aggregate_periods
 
 test_dir = os.path.dirname(__file__)
 
 class TestPeriods(unittest.TestCase):
     def test_featurization(self):
-        train_periods = pd.read_csv('data/periods_train_500000.csv')
-        train_periods = generate_periods_features(train_periods)
-        train_periods.set_index('item_id')
+        samples = pd.read_csv('data/train.csv', usecols=['item_id', 'user_id'])
+        train_periods = pd.read_csv('data/periods_train_500000.csv',
+                                    parse_dates=['date_to', 'date_from', 'activation_date'])
+        train_periods = get_aggregate_periods(all_periods=train_periods,
+                                              all_samples=samples,
+                                              write_to_csv=False)
+        # This returns empty df as the data is limited but it tests that the function runs through the end
+
 
 
 if __name__ == '__main__':
