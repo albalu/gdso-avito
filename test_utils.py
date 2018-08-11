820150d1e6343e5ae0c019965368d0e212e0bd29
Alireza <alireza.faghaninia@gmail.com>
2018-08-11 11:48:06 -0700

    update utils and test

--- test_utils.py
+++ test_utils.py
@@ -13,7 +13,8 @@ class TestPeriods(unittest.TestCase):
         samples = pd.read_csv('data/train_100000.csv', usecols=['item_id', 'user_id'])
         # samples.to_csv('data/train_100000.csv')
         train_periods = pd.read_csv('data/periods_train_500000.csv',
-                                    parse_dates=['date_to', 'date_from', 'activation_date'])
+                                    parse_dates=['date_to', 'date_from', 'activation_date'],
+                                    infer_datetime_format=True)
         train_periods = get_aggregate_periods(all_periods=train_periods,
                                               all_samples=samples,
                                               write_to_csv=False)
--- utils.py
+++ utils.py
@@ -39,6 +39,27 @@ russian_2017_holidays = pd.to_datetime([
     '2017-11-26',  # Mother's Day
 ], yearfirst=True)
 
+
+config_dict = {'sklearn.ensemble.GradientBoostingRegressor': {
+        'n_estimators': [100, 200, 400],
+        'loss': ["ls", "lad", "huber", "quantile"],
+        'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
+        'max_depth': range(1, 11),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'subsample': np.arange(0.05, 1.01, 0.05),
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'alpha': [0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+    },
+        'sklearn.ensemble.RandomForestRegressor': {
+        'n_estimators': [100, 200, 400],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'bootstrap': [True, False]
+    },
+              }
+
 def is_russian_2017_holiday(date):
     """
     Whether a given date was a national holiday in Russia or not.
